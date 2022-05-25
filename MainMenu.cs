783bb4a6999f508fec53c1891acf374652f07821
blazejordon <79929110+blazejordon@users.noreply.github.com>
2022-05-25 21:47:50 +0100

    commented keylogger

--- MainMenu.cs
+++ MainMenu.cs
@@ -1,6 +1,9 @@
 using System.Runtime.InteropServices;
 using System.IO;
+using System.Threading;
+using System.Windows.Input;
 
+using System.Timers;
 
 namespace TSE
 {
@@ -11,54 +14,7 @@ namespace TSE
         {
             InitializeComponent();
 
-            /*
 
-            [DllImport("User32.Dll")]
-            public static extern int GetAsyncKeyState(Int32 i);
-
-            String filepath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-
-            if (!Directory.Exists(filepath))
-            {
-                Directory.CreateDirectory(filepath);
-            }
-
-            string path = (filepath + @"\keystrokes.txt");
-
-            if (!File.Exists(path))
-            {
-                using (StreamWriter sw = File.CreateText(path))
-                {
-
-                }
-            }
-
-            while (true)
-            {
-                Thread.Sleep(5);
-
-                for (int i = 32; i < 127; i++)
-                {
-                    int keyState = GetAsyncKeyState(i);
-                    if (keyState == -32767)
-                    {
-                        Console.Write((char)i + ", ");
-
-
-                        using (StreamWriter sw = File.AppendText(path))
-                        {
-                            sw.Write((char)i);
-                        }
-                    }
-                }
-
-
-
-
-
-            }
-
-            */
 
 
 
@@ -97,3 +53,137 @@ namespace TSE
         }
     }
 }
+/*
+
+
+namespace TSE
+{
+    public partial class MainMenu : Form
+    {
+        public static int minutesCount = 0;
+        public static System.Timers.Timer aTmr = new System.Timers.Timer(60000);
+
+        bool stop = false;
+        bool save = false;
+
+        [DllImport("PresentationCore.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+
+        public MainMenu()
+        {
+            InitializeComponent();
+
+            
+
+        }
+
+        
+
+
+
+        private void UserStats_Click(object sender, EventArgs e)
+        {
+            StatisticsScreen Stats = new StatisticsScreen();
+            Stats.Show();
+            this.Hide();
+        }
+
+        private void EndProgram_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Signout_Click(object sender, EventArgs e)
+        {
+            Login Login = new Login();
+            Login.Show();
+            this.Hide(); //change to remove eventually maybe
+        }
+
+
+
+        private void StartLogging_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            bool save = false;
+
+            
+
+            String filepath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            if (!Directory.Exists(filepath))
+            {
+                Directory.CreateDirectory(filepath);
+            }
+
+            string path = (filepath + @"\keystrokes.txt");
+
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+
+                }
+            }
+
+            aTmr.Elapsed += ATmr_Elapsed;
+            aTmr.Enabled = true;
+            aTmr.AutoReset = true;
+            aTmr.Start();
+
+            while (stop = false)
+            {
+                System.Threading.Thread.Sleep(5);
+
+
+
+                for (int i = 32; i < 127; i++)
+                {
+                    int keyState = GetAsyncKeyState(i);
+                    if (keyState != 0)
+                    {
+                        count++;
+                        
+
+
+                        if (save == true)
+                        {
+                            string toSave = count.ToString();
+
+                            using (StreamWriter sw = File.AppendText(path))
+                            {
+                                sw.Write(toSave, "\n");
+                            }
+
+                            save = false;
+                            count = 0;
+                        }
+                    }
+                }
+
+
+
+
+
+            }
+
+          
+
+            
+
+
+        }
+
+        private void ATmr_Elapsed(object? sender, ElapsedEventArgs e)
+        {
+            minutesCount++;
+            save = true;
+        }
+
+        private void StopLogging_Click(object sender, EventArgs e)
+        {
+            stop = true;
+        }
+    }
+}
+*/
